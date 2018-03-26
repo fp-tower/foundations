5584ab598282ff0c8353f4de4a92e93ce7ccc8f0
Julien Truffaut <truffaut.julien@gmail.com>
2018-03-26 15:31:47 +0100

    add more exercises for ch1

--- src/main/scala/ch1/ExerciseCh1.scala
+++ src/main/scala/ch1/ExerciseCh1.scala
@@ -2,41 +2,66 @@ package ch1
 
 object ExerciseCh1 extends App {
 
-  // 1. Implement isAdult
-  def isAdult(i: Int): Boolean = ???
+  // 1a. is plus a pure function? why?
+  def plus(a: Int, b: Int): Int = a + b
 
+  // 1b. is div a pure function? why?
+  def div(a: Int, b: Int): Int =
+    if(b == 0) sys.error("Cannot divide by 0")
+    else a / b
 
+  // 1c. is times2 a pure function? why?
+  var counterTimes2 = 0
+  def times2(i: Int): Int = {
+    counterTimes2 += 1
+    i * 2
+  }
 
+  // 1d. is times3 a pure function? why?
+  def times3(i: Int): Int = {
+    println("do something here") // could be a database access or http call
+    i * 3
+  }
 
+  // 1e. is circleArea a pure function? why?
+  val pi = 3.14
+  def circleArea(radius: Double): Double =
+    radius * radius * pi
 
-  // 2. What if a user pass a negative number? e.g. isAdult(-5)
-  // how would update the definition / signature of isAdult
-
-
-
-
-
-  // 3a. What is the most precise type?
-  // Int           => Either[String, Boolean]
-  // Positive[Int] => Boolean
-
-
-
-  // 3b. Why? How do you define precision?
-
-
-
-
-
-  // 4.
+  // 1f. is inc or inc_v2 a pure function? why?
   def inc(xs: Array[Int]): Unit =
     for { i <- 0 until xs.length } xs(i) = xs(i) + 1
 
   def inc_v2(xs: Array[Int]): Unit =
     for { i <- xs.indices } xs(i) = xs(i) + 1
 
-  val foo = Array(1,2,3)
-  println(inc(foo))
-  println(foo.toList)
+
+
+
+
+  // 2a. Implement isAdult
+  def isAdult(i: Int): Boolean = ???
+
+
+
+
+
+  // 2b. What if a user pass a negative number? e.g. isAdult(-5)
+  // how would update the definition / signature of isAdult
+
+
+
+
+
+  // 2c. What is the most precise type?
+  // Int                  => Option[Boolean]
+  // Int Refined Positive => Boolean          (see https://github.com/fthomas/refined)
+
+
+
+  // 2d. Why? How do you define precision?
+
+
+
 
 }
