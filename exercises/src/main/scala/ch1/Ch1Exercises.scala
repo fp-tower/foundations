ef27e42d8cf0954fda05994f38e81ebd7b4817fc
Julien Truffaut <truffaut.julien@gmail.com>
2018-03-29 12:07:56 +0100

    add list exercises

--- exercises/src/main/scala/ch1/Ch1Exercises.scala
+++ exercises/src/main/scala/ch1/Ch1Exercises.scala
@@ -1,5 +1,7 @@
 package ch1
 
+import scala.util.Random
+
 object Ch1Exercises {
 
   // 1a. is plus a pure function? why?
@@ -17,29 +19,32 @@ object Ch1Exercises {
     i * 2
   }
 
-  // 1d. is mapLookup a pure function? why?
+  // 1d. is boolToInt a pure function? why?
+  def boolToInt(b: Boolean): Int =
+    if(b) 5
+    else Random.nextInt() / 2
+
+  // 1e. is mapLookup a pure function? why?
   def mapLookup(map: Map[String, Int], key: String): Int =
     map(key)
 
-  // 1d. is times3 a pure function? why?
+  // 1f. is times3 a pure function? why?
   def times3(i: Int): Int = {
     println("do something here") // could be a database access or http call
     i * 3
   }
 
-  // 1e. is circleArea a pure function? why?
+  // 1g. is circleArea a pure function? why?
   val pi = 3.14
   def circleArea(radius: Double): Double =
     radius * radius * pi
 
-  // 1f. is inc or inc_v2 a pure function? why?
+  // 1h. is inc or inc_v2 a pure function? why?
   def inc(xs: Array[Int]): Unit =
     for { i <- 0 to xs.length } xs(i) = xs(i) + 1
 
   def inc_v2(xs: Array[Int]): Unit =
     for { i <- xs.indices } xs(i) = xs(i) + 1
-
-
 
 
 }
---
+++ exercises/src/main/scala/ch4/ListExercises.scala
@@ -0,0 +1,79 @@
+package ch4
+
+object ListExercises {
+
+  sealed trait MyList[A]
+  object MyList {
+    case class MyNil[A]() extends MyList[A]
+    case class MyCons[A](head: A, tail: MyList[A]) extends MyList[A]
+  }
+
+  // a. Implement head
+  def head[A](xs: MyList[A]) = ???
+
+
+  // b. Implement last
+  def last[A](xs: MyList[A]) = ???
+
+
+  // c. Implement size
+  def size[A](xs: MyList[A]): Int = ???
+
+
+  // d. what issue do you see with the signature of size? How would fix it?
+
+
+
+  // e. Implement reverse
+  def reverse[A](xs: MyList[A]): MyList[A] = ???
+
+
+  // f. Implement map
+  def map[A, B](xs: MyList[A])(f: A => B): MyList[B] = ???
+
+
+  // g. Implement filter
+  def filter[A](xs: MyList[A])(f: A => Boolean): MyList[A] = ???
+
+
+  // h. Implement lookup
+  def lookup[A](xs: MyList[A])(i: Int): Option[A] = ???
+
+
+
+
+  // a. Implement foldLeft
+  def foldLeft[A, B](xs: MyList[A])(z: B)(f: (B, A) => B): B = ???
+
+
+  // b. Implement foldRight
+  def foldRight[A, B](xs: MyList[A])(z: B)(f: (A, => B) => B): B = ???
+
+
+  // c. what are the differences between foldLeft and foldRight
+  // hint: ???
+
+
+  // d. Implement sum in terms of a fold
+  def sum[A](xs: List[A]): Int = ???
+
+
+  // e. Implement reverse and map using a fold
+  def reverse_fold[A](xs: MyList[A]): MyList[A] = ???
+
+
+  def map_fold[A, B](xs: MyList[A])(f: A => B): MyList[B] = ???
+
+
+  // f. Implement filter using a fold
+  def filter_fold[A](xs: MyList[A])(f: A => Boolean): MyList[A] = ???
+
+
+
+  // g. Implement head and last using a fold
+  def head_fold[A](xs: MyList[A]): Option[A] = ???
+
+
+  def last_fold[A](xs: MyList[A]): Option[A] = ???
+
+}
