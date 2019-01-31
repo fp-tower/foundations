2062a652375f3232bcbfdf19043088be1dfed88b
Julien Truffaut <truffaut.julien@gmail.com>
2019-01-31 19:53:51 +0000

    add exercises about function vs methods

--- exercises/src/main/scala/function/FunctionExercises.scala
+++ exercises/src/main/scala/function/FunctionExercises.scala
@@ -71,61 +71,66 @@ object FunctionExercises extends App {
     acc
   }
 
-  // 2. higher order functions
+  // 2. polymorphic functions
 
-  // 2a. Implement andThen and compose
+  // 2a. Implement apply
+  def apply[A, B](f: A => B, value: A): B = ???
+
+  // 2b. Transform triple into a function (val). See Eta expansion https://stackoverflow.com/a/39446986
+
+  // 2c. Transform triple into a function (val)
+  def triple(x: Int): Int = x * 3
+
+
+  // 3. higher order functions
+
+  // 3a. Implement andThen and compose
   def andThen[A, B, C](f: A => B, g: B => C): A => C = ???
 
   def compose[A, B, C](f: B => C, g: A => B): A => C = ???
 
-  // 2b. Implement the function f(x) = 2 * x + 1 using inc, double with compose or andThen
+  // 3b. Implement the function f(x) = 2 * x + 1 using inc, double with compose or andThen
   val inc   : Int => Int = x => x + 1
   val double: Int => Int = x => 2 * x
 
 
-  // 2c. Same for f(x) = 2 * (x + 1)
+  // 3c. Same for f(x) = 2 * (x + 1)
 
 
-  // 2d. Implement curry and uncurry
+  // 3d. Implement curry and uncurry
   def curry[A, B, C](f: (A, B) => C): A => B => C = ???
 
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = ???
 
 
-  // 2e. Implement join
+  // 3e. Implement join
   def join[A, B, C, D](f: A => B, g: A => C)(h: (B, C) => D): A => D = ???
 
-  // 2f. Implement identity
+  // 3f. Implement identity
   def identity[A](x: A): A = ???
 
-  // 2g. Implement const
+  // 3g. Implement const
   def const[A, B](a: A)(b: B): A = ???
 
-  // 2h. Transform triple method (def) into a function (val)
-  def triple(x: Int): Int = x * 3
 
 
-  // 2i. Transform identity into a function (val). See Eta expansion https://stackoverflow.com/a/39446986
+  // 4. Recursion
 
-
-
-  // 3. Recursion
-
-  // 3a. Use recursion to implement sumList
+  // 4a. Use recursion to implement sumList
   def sumList(xs: List[Int]): Int = ???
 
 
-  // 3b. Run it with different size of list
+  // 4b. Run it with different size of list
   // What happens when the list is big enough? Why?
   // You can use `to` to generate a List from 1 to x
   val oneToTen = 1.to(10).toList
 
 
-  // 3c. Re-write sumList recursively without the same issue
+  // 4c. Re-write sumList recursively without the same issue
   def sumList2(xs: List[Int]): Int = ???
 
 
-  // 3d. Run isEven / isOdd for small and large input. Search for mutual tail recursion in scala
+  // 4d. Run isEven / isOdd for small and large input. Search for mutual tail recursion in scala
   def isEven(x: Int): Boolean =
     if(x > 0) !isOdd(x - 1)
     else if (x < 0) !isOdd(x + 1)
@@ -136,12 +141,12 @@ object FunctionExercises extends App {
     else if (x < 0) !isEven(x + 1)
     else false
 
-  // 3e. does the commented function below compile? If yes, what happens when you call it
+  // 4e. does the commented function below compile? If yes, what happens when you call it
   // see General recursion
-//  def foo: Int = foo
+  //  def foo: Int = foo
 
 
-  // 4a. Implement memoize such as
+  // 5a. Implement memoize such as
   // val cachedInc = memoize((_: Int) + 1)
   // cachedInc(3) // 4 calculated
   // cachedInc(3) // from cache
