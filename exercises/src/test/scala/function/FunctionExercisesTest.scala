f70fa24f63895d71741c2d5a550a781638265873
Julien Truffaut <truffaut.julien@gmail.com>
2019-02-22 16:20:31 +0000

    add tests for identity, const and join

--- exercises/src/test/scala/function/FunctionExercisesTest.scala
+++ exercises/src/test/scala/function/FunctionExercisesTest.scala
@@ -10,4 +10,21 @@ class FunctionExercisesTest extends FreeSpec with Matchers {
     doubleInc(6) shouldEqual 13
   }
 
+  "identity" in {
+    identity(3) shouldEqual 3
+    identity("foo") shouldEqual "foo"
+  }
+
+  "const" in {
+    const("foo")(5) shouldEqual "foo"
+    const(5)("foo") shouldEqual 5
+  }
+
+  "join" in {
+    val reverse: Boolean => Boolean = x => !x
+    val zeroOne: Boolean => String = x => if(x) "1" else "0"
+
+    join(zeroOne, reverse)(_ + _.toString)(true) shouldEqual "1false"
+  }
+
 }
