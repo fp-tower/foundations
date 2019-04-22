00137f391aacce9616623bd2149648544c0eedfa
Scala steward <me@scala-steward.org>
2019-04-22 03:28:20 +0200

    Update typesafe:config to 1.3.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val cats = "org.typelevel" %% "cats-core" % "1.6.0"
   lazy val refined = "eu.timepit" %% "refined" % "0.9.5"
-  lazy val typesafeConfig = "com.typesafe" % "config" % "1.3.3"
+  lazy val typesafeConfig = "com.typesafe" % "config" % "1.3.4"
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.7"
   lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
   lazy val discipline = "org.typelevel" %% "discipline" % "0.11.1"
