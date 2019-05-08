c4c34aed84a50b2cece8e397afa4b2309b192a18
Julien Truffaut <truffaut.julien@gmail.com>
2019-05-08 10:56:33 +0100

    move FlatMap to its own file

---
+++ exercises/src/main/scala/exercises/functors/FlatMap.scala
@@ -0,0 +1,12 @@
+package exercises.functors
+
+trait FlatMap[F[_]] extends Apply[F] {
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    flatMap(fa)(a => map(fb)(f(a, _)))
+}
+
+object FlatMap {
+  def apply[F[_]](implicit ev: FlatMap[F]): FlatMap[F] = ev
+}
