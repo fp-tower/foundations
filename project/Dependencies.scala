9e30beb861bcb1f65123ca3d028ec86e43369f95
Julien Truffaut <truffaut.julien@gmail.com>
2019-06-07 16:55:14 +0100

    update kind projector group id

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,12 +1,12 @@
 import sbt._
 
 object Dependencies {
-  lazy val cats           = "org.typelevel"  %% "cats-core"     % "2.0.0-M1"
-  lazy val catsEffect     = "org.typelevel"  %% "cats-effect"   % "1.3.1"
-  lazy val refined        = "eu.timepit"     %% "refined"       % "0.9.5"
-  lazy val typesafeConfig = "com.typesafe"   % "config"         % "1.3.4"
-  lazy val scalatest      = "org.scalatest"  %% "scalatest"     % "3.0.7"
-  lazy val scalacheck     = "org.scalacheck" %% "scalacheck"    % "1.14.0"
-  lazy val discipline     = "org.typelevel"  %% "discipline"    % "0.11.1"
-  lazy val kindProjector  = "org.spire-math" % "kind-projector" % "0.9.10" cross CrossVersion.binary
+  lazy val cats           = "org.typelevel"  %% "cats-core"      % "2.0.0-M1"
+  lazy val catsEffect     = "org.typelevel"  %% "cats-effect"    % "1.3.1"
+  lazy val refined        = "eu.timepit"     %% "refined"        % "0.9.5"
+  lazy val typesafeConfig = "com.typesafe"   % "config"          % "1.3.4"
+  lazy val scalatest      = "org.scalatest"  %% "scalatest"      % "3.0.7"
+  lazy val scalacheck     = "org.scalacheck" %% "scalacheck"     % "1.14.0"
+  lazy val discipline     = "org.typelevel"  %% "discipline"     % "0.11.1"
+  lazy val kindProjector  = "org.typelevel"  %% "kind-projector" % "0.10.1" cross CrossVersion.binary
 }
