73991f7e6e213c72ce2e0e5d974b6919c32a3e6a
Julien Truffaut <truffaut.julien@gmail.com>
2019-06-08 17:35:43 +0100

    Starts unrepresentable state exercises

--- exercises/src/main/scala/exercises/errorhandling/UnrepresentableStateExercises.scala
+++ exercises/src/main/scala/exercises/errorhandling/UnrepresentableStateExercises.scala
@@ -1,3 +1,33 @@
 package exercises.errorhandling
 
-object UnrepresentableStateExercises {}
+import java.time.Instant
+
+object UnrepresentableStateExercises {
+
+  ////////////////////////
+  // 1. Order
+  ////////////////////////
+
+  // All orders have an id and a status
+  // a status can be:
+  // * "Pending"   before checkout
+  // * "Submitted" sending to customer (cannot be empty)
+  // * "Cancelled" when a submitted ordered is cancelled
+  // * "Delivered" received by the customer (cannot be returned)
+  // submittedAt and cancelledAt record when an order has been submitted / cancelled
+  case class Order(
+    id: String,
+    status: String,
+    items: List[Item],
+    submittedAt: Option[Instant],
+    cancelledAt: Option[Instant]
+  )
+
+  case class Item(id: String, quantity: Int, unitPrice: Double)
+
+  // 1a. Implement total which calculate the total value of an order
+  def total(order: Order): Double = ???
+
+  // 1b. Implement submit
+  def submit(order: Order, now: Instant) = ???
+}
