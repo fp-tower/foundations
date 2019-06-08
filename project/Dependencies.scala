26744b32198962b6c3d073d994d15afc205ee330
Scala Steward <me@scala-steward.org>
2019-06-08 03:19:29 +0200

    Update cats-core to 2.0.0-M3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val cats           = "org.typelevel"  %% "cats-core"      % "2.0.0-M1"
+  lazy val cats           = "org.typelevel"  %% "cats-core"      % "2.0.0-M3"
   lazy val catsEffect     = "org.typelevel"  %% "cats-effect"    % "1.3.1"
   lazy val refined        = "eu.timepit"     %% "refined"        % "0.9.5"
   lazy val typesafeConfig = "com.typesafe"   % "config"          % "1.3.4"
