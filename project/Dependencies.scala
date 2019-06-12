61cd95e116f7f4c38e3dd2ebc0643c70e05e793f
Scala Steward <me@scala-steward.org>
2019-06-12 19:04:03 +0200

    Update refined to 0.9.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val cats           = "org.typelevel"  %% "cats-core"      % "2.0.0-M4"
   lazy val catsEffect     = "org.typelevel"  %% "cats-effect"    % "1.3.1"
-  lazy val refined        = "eu.timepit"     %% "refined"        % "0.9.7"
+  lazy val refined        = "eu.timepit"     %% "refined"        % "0.9.8"
   lazy val typesafeConfig = "com.typesafe"   % "config"          % "1.3.4"
   lazy val scalatest      = "org.scalatest"  %% "scalatest"      % "3.0.7"
   lazy val scalacheck     = "org.scalacheck" %% "scalacheck"     % "1.14.0"
