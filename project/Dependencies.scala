60bc6bf924c5e0964c7cc4b984c675602341a741
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-06-14 16:04:34 +0200

    Update scalatest to 3.0.8 (#59)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val catsEffect     = "org.typelevel"  %% "cats-effect"    % "1.3.1"
   lazy val refined        = "eu.timepit"     %% "refined"        % "0.9.8"
   lazy val typesafeConfig = "com.typesafe"   % "config"          % "1.3.4"
-  lazy val scalatest      = "org.scalatest"  %% "scalatest"      % "3.0.7"
+  lazy val scalatest      = "org.scalatest"  %% "scalatest"      % "3.0.8"
   lazy val scalacheck     = "org.scalacheck" %% "scalacheck"     % "1.14.0"
   lazy val discipline     = "org.typelevel"  %% "discipline"     % "0.11.1"
   lazy val kindProjector  = "org.typelevel"  %% "kind-projector" % "0.10.3" cross CrossVersion.binary
