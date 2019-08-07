c97549b412765a1bfd470ff8332eccbdb81aa4e2
Scala Steward <me@scala-steward.org>
2019-08-07 22:47:06 +0200

    Update cats-effect to 2.0.0-RC1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val cats           = "org.typelevel"  %% "cats-core"            % "2.0.0-RC1"
-  lazy val catsEffect     = "org.typelevel"  %% "cats-effect"          % "2.0.0-M5"
+  lazy val catsEffect     = "org.typelevel"  %% "cats-effect"          % "2.0.0-RC1"
   lazy val refined        = "eu.timepit"     %% "refined"              % "0.9.9"
   lazy val typesafeConfig = "com.typesafe"   % "config"                % "1.3.4"
   lazy val scalacheck     = "org.scalacheck" %% "scalacheck"           % "1.14.0"
