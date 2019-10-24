5a81edca6715fb7882bc54434f528f8769c076d7
Scala Steward <me@scala-steward.org>
2019-10-24 21:39:22 +0200

    Update discipline-scalatest to 1.0.0-RC1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,6 +9,6 @@ object Dependencies {
   lazy val circe          = "io.circe"       %% "circe-parser"         % "0.12.1"
   lazy val scalacheck     = "org.scalacheck" %% "scalacheck"           % "1.14.0"
   lazy val discipline     = "org.typelevel"  %% "discipline-core"      % "1.0.0"
-  lazy val disciplineTest = "org.typelevel"  %% "discipline-scalatest" % "1.0.0-M1"
+  lazy val disciplineTest = "org.typelevel"  %% "discipline-scalatest" % "1.0.0-RC1"
   lazy val kindProjector  = "org.typelevel"  %% "kind-projector"       % "0.10.3" cross CrossVersion.binary
 }
