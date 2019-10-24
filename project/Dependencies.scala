992ad1736530c4dca2a8c2136b5f634dd0af8dbd
Scala Steward <me@scala-steward.org>
2019-10-24 21:39:10 +0200

    Update discipline-core to 1.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val typesafeConfig = "com.typesafe"   % "config"                % "1.3.4"
   lazy val circe          = "io.circe"       %% "circe-parser"         % "0.12.1"
   lazy val scalacheck     = "org.scalacheck" %% "scalacheck"           % "1.14.0"
-  lazy val discipline     = "org.typelevel"  %% "discipline-core"      % "1.0.0"
+  lazy val discipline     = "org.typelevel"  %% "discipline-core"      % "1.0.1"
   lazy val disciplineTest = "org.typelevel"  %% "discipline-scalatest" % "1.0.0-M1"
   lazy val kindProjector  = "org.typelevel"  %% "kind-projector"       % "0.10.3" cross CrossVersion.binary
 }
