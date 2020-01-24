b3df0764b952bda2ec9a494e33f7a569006cbb03
Scala Steward <me@scala-steward.org>
2020-01-25 00:36:57 +0100

    Update refined to 0.9.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val cats           = "org.typelevel"  %% "cats-core"            % "2.1.0"
   lazy val catsFree       = "org.typelevel"  %% "cats-free"            % "2.1.0"
   lazy val catsEffect     = "org.typelevel"  %% "cats-effect"          % "2.0.0"
-  lazy val refined        = "eu.timepit"     %% "refined"              % "0.9.10"
+  lazy val refined        = "eu.timepit"     %% "refined"              % "0.9.12"
   lazy val circe          = "io.circe"       %% "circe-parser"         % "0.12.3"
   lazy val scalacheck     = "org.scalacheck" %% "scalacheck"           % "1.14.3"
   lazy val discipline     = "org.typelevel"  %% "discipline-core"      % "1.0.2"
