8b1e49d225721df5c6bbd4c84fa04fd64156c285
Scala Steward <me@scala-steward.org>
2020-02-08 02:20:18 +0100

    Update circe-parser to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val catsFree       = "org.typelevel"  %% "cats-free"            % "2.1.0"
   lazy val catsEffect     = "org.typelevel"  %% "cats-effect"          % "2.0.0"
   lazy val refined        = "eu.timepit"     %% "refined"              % "0.9.12"
-  lazy val circe          = "io.circe"       %% "circe-parser"         % "0.12.3"
+  lazy val circe          = "io.circe"       %% "circe-parser"         % "0.13.0"
   lazy val scalacheck     = "org.scalacheck" %% "scalacheck"           % "1.14.3"
   lazy val discipline     = "org.typelevel"  %% "discipline-core"      % "1.0.2"
   lazy val disciplineTest = "org.typelevel"  %% "discipline-scalatest" % "1.0.0-M1"
