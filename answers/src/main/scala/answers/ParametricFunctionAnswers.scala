6156a138d75a5071b9bbc3d3647ed1e3e2ee5e9d
Julien Truffaut <truffaut.julien@gmail.com>
2020-04-14 15:45:18 +0100

    move ParametricFunction up

--- answers/src/main/scala/answers/ParametricFunctionAnswers.scala
+++ answers/src/main/scala/answers/ParametricFunctionAnswers.scala
@@ -1,4 +1,4 @@
-package answers.function
+package answers
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
--- exercises/src/main/scala/exercises/ParametricFunctionExercises.scala
+++ exercises/src/main/scala/exercises/ParametricFunctionExercises.scala
@@ -1,7 +1,6 @@
-package exercises.function
+package exercises
 
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 object ParametricFunctionExercises {
 
