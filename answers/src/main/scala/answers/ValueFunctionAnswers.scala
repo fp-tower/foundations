88fb28d09c2ed9c338fa5ecfdd5ece1a22302609
Julien Truffaut <truffaut.julien@gmail.com>
2020-04-14 16:30:29 +0100

    add ValueFunction exercises and answers

---
+++ answers/src/main/scala/answers/ValueFunctionAnswers.scala
@@ -0,0 +1,41 @@
+package answers
+
+object ValueFunctionAnswers {
+
+  /////////////////////////////////////////////////////
+  // Exercise 1: String API with higher-order functions
+  /////////////////////////////////////////////////////
+
+  def selectDigits(text: String): String =
+    text.filter(_.isDigit)
+
+  def secret(text: String): String =
+    text.map(_ => '*')
+
+  def isValidUsernameCharacter(char: Char): Boolean =
+    char.isLetterOrDigit || (char == '-') || (char == '_')
+
+  def isValidUsername(username: String): Boolean =
+    username.forall(isValidUsernameCharacter)
+
+  ///////////////////////
+  // Exercise 2: Point3
+  ///////////////////////
+
+  case class Point3(x: Int, y: Int, z: Int) {
+    def isPositive: Boolean =
+      x >= 0 && y >= 0 && z >= 0
+
+    def isEven: Boolean =
+      (x % 2 == 0) && (y % 2 == 0) && (z % 2 == 0)
+
+    def forAll(predicate: Int => Boolean): Boolean =
+      predicate(x) && predicate(y) && predicate(z)
+
+    def isPositiveForAll: Boolean =
+      forAll(_ >= 0)
+
+    def isEvenForAll: Boolean =
+      forAll(_ % 2 == 0)
+  }
+}
---
+++ exercises/src/main/scala/exercises/ValueFunctionExercises.scala
@@ -0,0 +1,68 @@
+package exercises
+
+object ValueFunctionExercises {
+
+  /////////////////////////////////////////////////////
+  // Exercise 1: String API with higher-order functions
+  /////////////////////////////////////////////////////
+
+  // 1a. Implement `selectDigits` which iterates over a String and only keep the characters that are digits.
+  // such as selectDigits("123foo0-!Bar~+3") == "12303"
+  // but     selectDigits("hello") == ""
+  // Note: You can use `filter` method from `String`, also check out the API of Char
+  def selectDigits(text: String): String =
+    ???
+
+  // 1b. Implement `secret` which transforms all characters in a String to '*'
+  // such as secret("Welcome123") == "**********"
+  // Note: You can use `map` method from `String`
+  def secret(text: String): String =
+    ???
+
+  // 1c. Implement `isValidUsernameCharacter` which checks if a character is suitable for a username.
+  // We accept:
+  // - lower and upper case letters
+  // - digits
+  // - special characters: '-' and '_'
+  // For example, isValidUsernameCharacter('3') == true
+  //              isValidUsernameCharacter('a') == true
+  // but          isValidUsernameCharacter('^') == false
+  def isValidUsernameCharacter(char: Char): Boolean =
+    ???
+
+  // 1d. Implement `isValidUsername` which checks that all the characters in a String are valid
+  // such as isValidUsername("john-doe") == true
+  // but     isValidUsername("*john*") == false
+  // Note: You can use `forAll` method from `String`
+  def isValidUsername(username: String): Boolean =
+    ???
+
+  ///////////////////////
+  // Exercise 2: Point3
+  ///////////////////////
+
+  case class Point3(x: Int, y: Int, z: Int) {
+    // 2a. Implement `isPositive` which returns true if both `x` and `y` are greater or equal to 0, false otherwise
+    // such as Point3(2, 3, 9).isPositive == true
+    //         Point3(0, 0, 0).isPositive == true
+    // but     Point3(0,-2,-1).isPositive == false
+    // Note: `isPositive` is a function defined within `Point` class, so `isPositive` has access to `x`, `y` and `z`.
+    def isPositive: Boolean =
+      ???
+
+    // 2b. Implement `isEven` which returns true if both `x`, `y`, and `z` are even numbers, false otherwise
+    // such as Point(2, 4, 8).isEven == true
+    // such as Point(0,-8,-2).isEven == true
+    // but     Point(3,-2, 0).isEven == false
+    def isEven: Boolean =
+      ???
+
+    // 2c. Both `isPositive` and `isEven` check that a predicate holds for both `x` and `y`.
+    // Let's try to capture this pattern with a higher order function like `forAll`
+    // such as Point(1,1,1).forAll(_ == 1) == true
+    // but     Point(1,2,5).forAll(_ == 1) == false
+    // Then, re-implement `isPositive` and `isEven` using `forAll`
+    def forAll(predicate: Int => Boolean): Boolean =
+      ???
+  }
+}
