be959cf91e4e7fe7d7d8c775f2a95755baf534db
Julien Truffaut <truffaut.julien@gmail.com>
2020-04-29 12:57:18 +0100

    add shell for parametric functions exercises

---
+++ exercises/src/test/scala/function/ParametricFunctionExercisesTest.scala
@@ -0,0 +1,24 @@
+package function
+
+import exercises.function.ParametricFunctionExercises._
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+class ParametricFunctionExercisesTest extends AnyFunSuite with Matchers with ScalaCheckDrivenPropertyChecks {
+
+  ////////////////////
+  // Exercise 1: Pair
+  ////////////////////
+
+  test("Pair swap") {}
+
+  ////////////////////////////
+  // Exercise 2: Predicate
+  ////////////////////////////
+
+  ////////////////////////////
+  // Exercise 3: JsonDecoder
+  ////////////////////////////
+
+}
