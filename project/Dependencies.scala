edf17aa2976b793915339d0652d964783d1be6b8
Scala Steward <me@scala-steward.org>
2020-08-31 04:19:59 +0200

    Update scalacheck-1-14 to 3.1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 object Dependencies {
-  val scalatest     = "org.scalatestplus" %% "scalacheck-1-14" % "3.1.2.0" % Test
+  val scalatest     = "org.scalatestplus" %% "scalacheck-1-14" % "3.1.4.0" % Test
   val kindProjector = "org.typelevel"     %% "kind-projector"  % "0.10.3" cross CrossVersion.binary
 }
