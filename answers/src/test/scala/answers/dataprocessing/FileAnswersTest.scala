1774cb8d6e1784b79d8f2e431001c79999c96866
Julien Truffaut <truffaut.julien@gmail.com>
2020-09-18 17:25:41 +0100

    ignore all file tests

--- answers/src/test/scala/answers/dataprocessing/FileAnswersTest.scala
+++ answers/src/test/scala/answers/dataprocessing/FileAnswersTest.scala
@@ -9,16 +9,16 @@ import FileAnswers._
 class FileAnswersTest extends AnyFunSuite with ScalaCheckDrivenPropertyChecks {
   val scalaSeedProject = new File("/Users/julien/project/foundations/answers/src/main/resources/scala-seed-project")
 
-  test("diskUsage") {
+  ignore("diskUsage") {
     assert(diskUsageImperative(scalaSeedProject) == 1986)
     assert(diskUsage(scalaSeedProject) == 1986)
   }
 
-  test("largestFileSize") {
+  ignore("largestFileSize") {
     assert(largestFileSize(scalaSeedProject) == 447)
   }
 
-  test("filterFiles") {
+  ignore("filterFiles") {
     assert(
       filterFiles(scalaSeedProject, _.getName.endsWith(".scala")).map(_.getName).sorted ==
         List("Dependencies.scala", "Hello.scala", "HelloSpec.scala")
