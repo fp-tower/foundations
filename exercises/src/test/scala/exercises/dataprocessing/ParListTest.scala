572e1dd4f6ecce52a95709f6591e991ecdbd156f
Julien Truffaut <truffaut.julien@gmail.com>
2020-09-19 15:20:05 +0100

    add unit test

--- exercises/src/test/scala/exercises/dataprocessing/ParListTest.scala
+++ exercises/src/test/scala/exercises/dataprocessing/ParListTest.scala
@@ -6,7 +6,23 @@ import TemperatureExercises._
 
 class ParListTest extends AnyFunSuite with ScalaCheckDrivenPropertyChecks {
 
-  test("minSampleByTemperature") {}
+  test("minSampleByTemperature") {
+    val samples = List(
+      Sample("Africa", "Algeria", None, "Algiers", 8, 1, 2020, 50),
+      Sample("Africa", "Algeria", None, "Algiers", 8, 1, 2020, 56.3),
+      Sample("Africa", "Algeria", None, "Algiers", 8, 1, 2020, 23.4),
+      Sample("Africa", "Algeria", None, "Algiers", 8, 1, 2020, 89.7),
+      Sample("Africa", "Algeria", None, "Algiers", 8, 1, 2020, 22.1),
+      Sample("Africa", "Algeria", None, "Algiers", 8, 1, 2020, 34.7),
+      Sample("Africa", "Algeria", None, "Algiers", 8, 1, 2020, 99.0),
+    )
+    val parSamples = ParList.byPartitionSize(3, samples)
+
+    assert(
+      minSampleByTemperature(parSamples) ==
+        Some(Sample("Africa", "Algeria", None, "Algiers", 8, 1, 2020, 22.1))
+    )
+  }
 
   test("averageTemperature") {}
 }
