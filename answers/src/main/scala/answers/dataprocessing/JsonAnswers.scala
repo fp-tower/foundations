be7e91c0d3379c01c466c5b6b2a7b7f9ad45709a
Asjad Baig <asjad.baig.ind@gmail.com>
2020-11-17 00:05:22 +0530

    JsonBoolean addition to the answers section (#160)
    * JsonBoolean addition to the answers section
    
    * JsonBoolean value shouldn't be altered by anonymize

--- answers/src/main/scala/answers/dataprocessing/JsonAnswers.scala
+++ answers/src/main/scala/answers/dataprocessing/JsonAnswers.scala
@@ -4,6 +4,7 @@ object JsonAnswers {
 
   sealed trait Json
   case class JsonNumber(number: Double)         extends Json
+  case class JsonBoolean(bool: Boolean)         extends Json
   case class JsonString(text: String)           extends Json
   case class JsonObject(obj: Map[String, Json]) extends Json
   case class JsonArray(array: List[Json])       extends Json
@@ -11,7 +12,7 @@ object JsonAnswers {
 
   def trimAll(json: Json): Json =
     json match {
-      case _: JsonNumber   => json
+      case _: JsonNumber | _: JsonBoolean  => json
       case JsonString(str) => JsonString(str.trim)
       case JsonObject(obj) =>
         JsonObject(obj.map {
@@ -24,6 +25,7 @@ object JsonAnswers {
   def anonymize(json: Json): Json =
     json match {
       case _: JsonNumber => JsonNumber(0)
+      case _: JsonBoolean => json
       case _: JsonString => JsonString("***")
       case JsonObject(obj) =>
         JsonObject(obj.map {
@@ -38,7 +40,7 @@ object JsonAnswers {
     if (maxDepth < 0) false
     else
       json match {
-        case _: JsonNumber | JsonNull => false
+        case _: JsonNumber | _: JsonBoolean | JsonNull => false
         case JsonString(text)         => text.contains(searchText)
         case JsonObject(obj)          => obj.values.exists(search(_, searchText, maxDepth - 1))
         case JsonArray(array)         => array.exists(search(_, searchText, maxDepth - 1))
@@ -46,7 +48,7 @@ object JsonAnswers {
 
   def depth(json: Json): Int =
     json match {
-      case _: JsonNumber | _: JsonString | JsonNull =>
+      case _: JsonNumber | _: JsonString | _: JsonBoolean | JsonNull =>
         0
       case JsonObject(obj) =>
         obj.values.map(depth).maxOption.fold(0)(_ + 1)
