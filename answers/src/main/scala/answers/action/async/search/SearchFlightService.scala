9747b0c6953db290db1af90c32768d1f6a30194a
Julien Truffaut <julien@fp-tower.com>
2021-05-04 10:56:53 +0100

    simplify code of SearchFlightService

--- answers/src/main/scala/answers/action/async/search/SearchFlightService.scala
+++ answers/src/main/scala/answers/action/async/search/SearchFlightService.scala
@@ -14,24 +14,24 @@ object SearchFlightService {
 
   def fromClients(clients: List[SearchFlightClient])(ec: ExecutionContext): SearchFlightService =
     new SearchFlightService {
-      val wrappedClients = clients.map(wrappedClient(_)(ec))
+      def search(from: Airport, to: Airport, date: LocalDate): IO[SearchResult] = {
+        def searchPerClient(client: SearchFlightClient): IO[List[Flight]] =
+          client
+            .search(from, to, date)
+            .map(removeInvalidFlights)
+            .timeout(Duration.ofMillis(100))(ec)
+            .handleErrorWith(_ => IO(Nil))
 
-      def search(from: Airport, to: Airport, date: LocalDate): IO[SearchResult] =
-        wrappedClients
-          .parTraverse(_.search(from, to, date))(ec)
+        def removeInvalidFlights(flights: List[Flight]): List[Flight] =
+          flights.filter { flight =>
+            flight.from == from && flight.to == to && flight.departureDate == date
+          }
+
+        clients
+          .parTraverse(searchPerClient)(ec)
           .map(_.flatten)
           .map(SearchResult(_))
+      }
     }
 
-  def wrappedClient(client: SearchFlightClient)(ec: ExecutionContext): SearchFlightClient =
-    new SearchFlightClient {
-      def search(from: Airport, to: Airport, date: LocalDate): IO[List[Flight]] =
-        client
-          .search(from, to, date)
-          .map(_.filter { flight =>
-            flight.from == from && flight.to == to && flight.departureDate == date
-          })
-          .timeout(Duration.ofMillis(100))(ec)
-          .handleErrorWith(_ => IO(Nil))
-    }
 }
