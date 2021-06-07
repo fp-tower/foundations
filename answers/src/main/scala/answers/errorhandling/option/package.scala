937998602fa116c22d3519df404ecdb6db60b45d
Julien Truffaut <julien@fp-tower.com>
2021-06-07 13:50:16 +0100

    merge option exercises into one file

--- answers/src/main/scala/answers/errorhandling/option/package.scala
+++ answers/src/main/scala/answers/errorhandling/option/package.scala
@@ -4,12 +4,12 @@ package object option {
 
   implicit class ListOptionExtension[A](options: List[Option[A]]) {
     def sequence: Option[List[A]] =
-      Exercise3.sequence(options)
+      OptionAnswers.sequence(options)
   }
 
   implicit class ListExtension[A](values: List[A]) {
     def traverse[B](transform: A => Option[B]): Option[List[B]] =
-      Exercise3.traverse(values)(transform)
+      OptionAnswers.traverse(values)(transform)
   }
 
 }
