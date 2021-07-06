179e5aa8282dadb60d1cdc46acbc6e63f26df701
Julien Truffaut <julien@fp-tower.com>
2021-07-06 17:31:36 +0100

    fix issue

--- answers/src/main/scala/answers/errorhandling/domain/package.scala
+++ answers/src/main/scala/answers/errorhandling/domain/package.scala
@@ -3,18 +3,18 @@ import answers.action.async.IO
 
 package object domain {
 
-  implicit case class IOOptionExtension[A](self: IO[Option[A]]) {
+  implicit class IOOptionExtension[A](self: IO[Option[A]]) {
     def getOrFail(error: Exception): IO[A] =
-      self.flatMap{
+      self.flatMap {
         case Some(value) => IO(value)
-        case None =>IO.fail(error)
+        case None        => IO.fail(error)
       }
   }
 
-  implicit case class EitherExtension[A](self: Either[Throwable, A]) {
+  implicit class EitherExtension[A](self: Either[Throwable, A]) {
     def getOrFail: IO[A] =
       self match {
-        case Left(value) => IO.fail(value)
+        case Left(value)  => IO.fail(value)
         case Right(value) => IO(value)
       }
   }
