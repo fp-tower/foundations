a8187aeb62f790b3cab322c08728fb4d4de6ba29
Julien Truffaut <julien@fp-tower.com>
2021-07-08 16:49:25 +0100

    review Option

--- exercises/src/test/scala/exercises/errorhandling/option/OptionExercisesTest.scala
+++ exercises/src/test/scala/exercises/errorhandling/option/OptionExercisesTest.scala
@@ -15,14 +15,6 @@ class OptionExercisesTest extends AnyFunSuite with ScalaCheckDrivenPropertyCheck
     assert(getAccountId(Admin) == None)
   }
 
-  ignore("asEditor example") {
-    val accountId = AccountId(124)
-
-    assert(asEditor(Reader(accountId, true)) == None)
-    assert(asEditor(Editor(accountId, "Comic Sans")) == Some(Editor(accountId, "Comic Sans")))
-    assert(asEditor(Admin) == None)
-  }
-
   ignore("getUserEmail example") {
     val users = Map(
       UserId(222) -> User(UserId(222), "john", Admin, Some(Email("j@x.com"))),
@@ -50,11 +42,9 @@ class OptionExercisesTest extends AnyFunSuite with ScalaCheckDrivenPropertyCheck
       checkAllEmails(
         List(
           User(UserId(111), "Eda", Editor(AccountId(555), "Comic Sans"), Some(Email("e@y.com"))),
-          User(UserId(222), "Bob", Reader(AccountId(555), true), None),
-          User(UserId(333), "Lea", Reader(AccountId(741), false), None),
           User(UserId(444), "Jo", Admin, Some(Email("admin@fp-tower.com")))
         )
-      ) == None
+      ) == Some(List(Email("e@y.com"), Email("admin@fp-tower.com")))
     )
   }
 
@@ -74,5 +64,13 @@ class OptionExercisesTest extends AnyFunSuite with ScalaCheckDrivenPropertyCheck
   ignore("sequence example") {
     assert(sequence(List(Some(1), Some(2), Some(3))) == Some(List(1, 2, 3)))
     assert(sequence(List(Some(1), None, Some(3))) == None)
+  }
+
+  ignore("asEditor example") {
+    val accountId = AccountId(124)
+
+    assert(asEditor(Reader(accountId, true)) == None)
+    assert(asEditor(Editor(accountId, "Comic Sans")) == Some(Editor(accountId, "Comic Sans")))
+    assert(asEditor(Admin) == None)
   }
 }
