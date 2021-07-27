0ea875cd09fd2454ad63b8458e4db0cb42e1a9ab
Julien Truffaut <julien@fp-tower.com>
2021-07-27 17:32:08 +0100

    add test for order project

--- exercises/src/test/scala/exercises/errorhandling/project/OrderTest.scala
+++ exercises/src/test/scala/exercises/errorhandling/project/OrderTest.scala
@@ -1,5 +1,6 @@
 package exercises.errorhandling.project
 
+import exercises.errorhandling.project.OrderError.{EmptyBasket, InvalidStatus}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
@@ -7,11 +8,56 @@ import java.time.{Duration, Instant}
 
 class OrderTest extends AnyFunSuite with ScalaCheckDrivenPropertyChecks {
 
+  ignore("checkout successful example") {
+    val order = Order(
+      id = "AAA",
+      status = "Draft",
+      basket = List(Item("A1", 2, 12.99)),
+      deliveryAddress = None,
+      createdAt = Instant.now(),
+      submittedAt = None,
+      deliveredAt = None
+    )
+
+    order.checkout match {
+      case Left(value)     => fail(s"Expected success but got $value")
+      case Right(newOrder) => assert(newOrder.status == "Checkout")
+    }
+  }
+
+  ignore("checkout empty basket example") {
+    val order = Order(
+      id = "AAA",
+      status = "Draft",
+      basket = Nil,
+      deliveryAddress = None,
+      createdAt = Instant.now(),
+      submittedAt = None,
+      deliveredAt = None
+    )
+
+    assert(order.checkout == Left(EmptyBasket))
+  }
+
+  ignore("checkout invalid status example") {
+    val order = Order(
+      id = "AAA",
+      status = "Delivered",
+      basket = List(Item("A1", 2, 12.99)),
+      deliveryAddress = None,
+      createdAt = Instant.now(),
+      submittedAt = None,
+      deliveredAt = None
+    )
+
+    assert(order.checkout == Left(InvalidStatus("Delivered")))
+  }
+
   ignore("happy path") {
     val orderId         = "ORD0001"
     val createdAt       = Instant.now()
     val submittedAt     = createdAt.plusSeconds(5)
-    val deliveredAt     = createdAt.plusSeconds(3600 * 30) // 30 hours
+    val deliveredAt     = submittedAt.plusSeconds(3600 * 30) // 30 hours
     val order           = Order.empty(orderId, createdAt)
     val item1           = Item("AAA", 2, 24.99)
     val item2           = Item("BBB", 1, 15.49)
