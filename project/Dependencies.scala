a6539f4c3623b85202464fda81fde755a356bf4e
Scala Steward <me@scala-steward.org>
2021-09-30 12:24:30 +0200

    Update kantan.csv to 0.6.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val kantanGeneric = "com.nrinaudo"      %% "kantan.csv"      % "0.6.1"
+  val kantanGeneric = "com.nrinaudo"      %% "kantan.csv"      % "0.6.2"
   val scalatest     = "org.scalatestplus" %% "scalacheck-1-14" % "3.1.2.0" % Test
   val kindProjector = "org.typelevel"     %% "kind-projector"  % "0.10.3" cross CrossVersion.binary
 }
