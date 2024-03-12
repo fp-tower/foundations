073dba246acd279e90747d2eb181ecb4ded48946
Julien Truffaut <julien@fp-tower.com>
2024-03-12 16:29:54 +0100

    Bump dependencies (#265)
    * bump dependencies
    
    * add scalatest
    
    ---------
    
    Co-authored-by: Julien Truffaut <julien@scalajobs.dev>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,8 @@
 import sbt._
 
 object Dependencies {
-  val kantanGeneric = "com.nrinaudo"      %% "kantan.csv"      % "0.6.1"
-  val scalatest     = "org.scalatestplus" %% "scalacheck-1-14" % "3.1.2.0" % Test
-  val kindProjector = "org.typelevel"     %% "kind-projector"  % "0.10.3" cross CrossVersion.binary
+  val kantanGeneric = "com.nrinaudo"      %% "kantan.csv"      % "0.7.0"
+  val scalatest     = "org.scalatest"     %% "scalatest"       % "3.2.18"  % Test
+  val scalacheck    = "org.scalatestplus" %% "scalacheck-1-17" % "3.2.18.0" % Test
+  val kindProjector = "org.typelevel"     %% "kind-projector"  % "0.13.3" cross CrossVersion.binary
 }
